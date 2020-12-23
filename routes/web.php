37cfbd305cadc033cc071bbd8ac476e4c42b065a
moshoodfakorede <moshood@armpension.com>
2020-12-23 19:39:41 +0100

    dashboard template setup

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
